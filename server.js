a96d03576f927f9064b1e63cfa4235d60c0530dd
arnaudMaillet <arnaud.maillet@saint-remi.net>
2021-04-15 23:32:03 +0200

    Done  : Register Post

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const mysql = require('mysql')
+const cors = require('cors')
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const db = mysql.createConnection({
     user: "root",
     host: "localhost",
     password: "",
-    database: "LoginSystem"
+    database: "loginsystem"
+})
+
+app.post('/register', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    db.query(
+        "INSERT INTO user (username, password) values (?,?)",
+        [username, password],
+        (err, result) => {
+            console.log(err);
+        }
+    )
+    console.log(username, password);
 })
 
 app.get('/', (req, res)=>{
