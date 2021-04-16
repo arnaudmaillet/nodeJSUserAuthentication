3ea53c7e0ebc822950af92d791cdca7701f92d8a
arnaudMaillet <arnaud.maillet@saint-remi.net>
2021-04-16 21:57:22 +0200

    Add : Bcrypt package

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
-const mysql = require('mysql')
-const cors = require('cors')
+const mysql = require('mysql');
+const cors = require('cors');
 
+// Init Bcryot (Encryption package)
+const bcrypt = require('bcrypt')
+const salt = 10
+
+// Init const express
 const app = express();
+
 
 app.use(express.json());
 app.use(cors());
 
+
+// DataBase connection
 const db = mysql.createConnection({
     user: "root",
     host: "localhost",
@@ -14,24 +22,44 @@ const db = mysql.createConnection({
     database: "loginsystem"
 })
 
+
+// Register
 app.post('/register', (req, res) => {
     const username = req.body.username
     const password = req.body.password
 
+    bcrypt.hash(password, salt, (err, hash) => {
+        db.query(
+            "INSERT INTO user (username, password) VALUES (?,?)",
+            [username, hash],
+            (err, result) => {
+                err ? res.send({err: err}) : null;
+                result ? res.send({message: "Vous êtes bien enregistré"}) : null;
+            }
+        )
+    })
+})
+
+
+//Login
+app.post('/login', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
     db.query(
-        "INSERT INTO user (username, password) values (?,?)",
-        [username, password],
+        "SELECT username, password FROM user WHERE username = ?;",
+        username,
         (err, result) => {
-            console.log(err);
+            err ? res.send({err: err}) : null;
+            result.length > 0 ? bcrypt.compare(password, result[0].password, (error, response) => {
+                response ? res.send(result) : res.send({message: "Login ou mot de passe invalide !"})
+            }) : res.send({message: "Utilisateur non connu !"});
         }
     )
-    console.log(username, password);
 })
 
-app.get('/', (req, res)=>{
-    res.send("Hello Word")
-})
 
+// Port output config
 app.listen(3003, ()=>{
     console.log("Running server => port 3003");
 })
